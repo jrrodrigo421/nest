818cb69511adc0f4753182979e1a1546fad3d4e2
jrrodrigo42 <rjr89000@gmail.com>
2026-02-18 14:39:57 -0300

    feat: tests modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { HelloWorldModule } from './resources/helloworld/helloworld.module';
+// import { HelloWorldModule } from './resources/helloworld/helloworld.module';
+import { CatsModule } from './resources/cat/cats.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(HelloWorldModule, { abortOnError: false })
+  const app = await NestFactory.create(CatsModule, { abortOnError: false })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/resources/cat/cats.controller.ts
+++ src/resources/cat/cats.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from "@nestjs/common"
 
 @Controller('cats')
 export class CatsController {
-  @Get()
+  @Get('')
   findAll(): string {
     return "teste gats module"
   }
