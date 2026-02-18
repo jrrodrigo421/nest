79d1f58a75d4964a7a42c39b363b46d2a355ac6a
jrrodrigo42 <rjr89000@gmail.com>
2026-02-18 15:34:30 -0300

    feat: tests in reqs node, express and fastify

--- src/resources/cat/cats.controller.ts
+++ src/resources/cat/cats.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Req } from '@nestjs/common';
+import type { Request } from 'express';
+// import type { FastifyRequest } from 'fastify';
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(): string {
+  // findAll(@Req() request: FastifyRequest): string {
+  findAll(@Req() request: Request): string {
+    // console.log(request.hostname);  // fastify
+    console.log(request.get('host'));  // express
+    console.log(request.body);  // Node
     return 'teste gats module';
   }
 }
