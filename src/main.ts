4ddb7ab13015a99645cf5eb9373271279a483dca
jrrodrigo42 <rjr89000@gmail.com>
2026-02-18 19:36:40 -0300

    feat: complex query witch fastify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { CatsModule } from './resources/cat/cats.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import * as qs from 'qs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(CatsModule, {
-    abortOnError: false,
-  });
+  // const app = await NestFactory.create<NestExpressApplication>(CatsModule, {
+  const app = await NestFactory.create<NestFastifyApplication>(CatsModule,
+    //    {
+    //   abortOnError: false,
 
-  // Ativa o parser de query estendido (suporta objetos e matrizes)
-  app.set('query parser', 'extended');
+    // },
+    new FastifyAdapter({
+      querystringParser: (str) => qs.parse(str),
+    }),);
+
+  // Ativa o parser de query estendido (suporta objetos e matrizes) -> ISSO PRA EXPRESS
+  // app.set('query parser', 'extended');
+
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
